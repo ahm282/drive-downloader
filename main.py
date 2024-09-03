e7340c9eede35dc267b76acbd86d1bc97e090a1f
Ahmed Mahgoub <112783652+ahm282@users.noreply.github.com>
2024-09-03 11:11:35 +0300

    feat: add linux check for downloads and now creates subfolders - refactor: removed duplicate logic from main and reworked functions in download_files

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+from sys import platform
 from dotenv import load_dotenv
 import authenticate_account
 import download_files
+
 
 def main():
     # Authenticate the account (login) and load the service
@@ -11,38 +13,39 @@ def main():
     # FIXME: On Windows and some Linux distributions, the Downloads folder is localised.
     # This works only on when the downloads folder is actually named "Downloads".
     # User can set the destination folder to a custom path.
-    dest_folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
 
-    # Custom path
-    # dest_folder = 'C:\\Users\\user\\Downloads'
+    try:
+        if platform in ["linux", "darwin"]:
+            dest_folder = os.path.join(os.path.join(os.environ["HOME"]), "Downloads")
+        elif platform in ["win32", "cygwin"]:
+            dest_folder = os.path.join(
+                os.path.join(os.environ["USERPROFILE"]), "Downloads"
+            )
+
+    except (KeyError, OSError) as e:
+        print(f"Error: {e}")
+        print("Please enter a path to save the files.")
+        dest_folder = input("Destination: ")
+
+        # # Custom path
+        # dest_folder = 'C:\\Users\\user\\Downloads'
 
     # Load environment variables from .env file
     load_dotenv()
 
     # Query for the folder id
-    folder_id = os.getenv('FOLDER_ID')
+    folder_id = os.getenv("FOLDER_ID")
 
     if not folder_id:
-        print('Please set the FOLDER_ID environment variable.')
+        print("Please set the FOLDER_ID environment variable.")
         return
 
-    query = f"'{folder_id}' in parents"
-
-   # List the shared folders
+    # List all files
     try:
-        results = service.files().list(q=query, includeItemsFromAllDrives=True, supportsAllDrives=True, fields="nextPageToken, files(id, name)").execute()
-        items = results.get('files', [])
-
-        if not items:
-            print('No files found in the folder.')
-        else:
-            print(f"Found {len(items)} files in the folder.")
-            for item in reversed(items):
-                # FIXME: mkdir subfolders
-                download_files.download_all_files_in_folder(service, item['id'], dest_folder)
-
+        download_files.download_all_files_in_folder(service, folder_id, dest_folder)
     except Exception as e:
-        print(f'An error occurred: {e}')
+        print(f"An error occurred: {e}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
